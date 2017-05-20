ea80381ed632fb49c9eec8543ed391affb4d85d1
service <servicemanager3386@yandex.com>
2017-05-20 16:01:20 -0400

    # issue: need pulling

--- adidasapi/urls.py
+++ adidasapi/urls.py
@@ -15,9 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from apis import urls
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include(urls)),
+    url(r'^', include(apis.urls)),
 ]
