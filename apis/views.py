aaffef7aa8576c12bb7c037805a92f4a91f1076c
service <servicemanager3386@yandex.com>
2017-05-22 09:19:03 -0400

    # create:  - custom permission for user who created object, to do action with CUD

--- apis/views.py
+++ apis/views.py
@@ -4,6 +4,7 @@ from rest_framework import generics, permissions
 
 from apis.models import Snippet
 from apis.serializers import SnippetSerializer, UserSerializer
+from apis.permissions import IsOwnerOrReadOnly
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
@@ -16,7 +17,8 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly,)
 
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
